cf09526d05107a9d5acaf002885a189aab594a3a
PurinSiripru <122721492+PurinSiripru@users.noreply.github.com>
2024-11-29 10:51:00 +0700

    Update delete.spec.js

--- cypress/e2e/delete.spec.js
+++ cypress/e2e/delete.spec.js
@@ -1,9 +1,9 @@
 describe("Delete Company Test",()=>{
     it("enter company",()=>{
         cy.visit("https://vending-main-client-dev.rvvcs.com/auth/login"); 
-        cy.get("#input-0").type('admin');
-        cy.get("#input-2").type('123456');
-        cy.get(`#checkbox-4`).check();
+        cy.get('input[class="v-field__input"]').eq(0).type('admin');
+        cy.get('input[class="v-field__input"]').eq(1).type('123456');
+        cy.get('input[type="checkbox"]').check();
         cy.get("#__nuxt > div > div > div > div > div > div > div > div.v-card-item.py-8.pa-sm-8.pa-md-15 > div > div > div:nth-child(2) > div:nth-child(4) > button").click()
         cy.get("#__nuxt > div > div > div > div > nav")
         cy.get("#__nuxt > div > div > div > div > div").wait(2000).click()
@@ -14,9 +14,8 @@ describe("Delete Company Test",()=>{
         cy.get('#__nuxt > div > div > div > div > main > div > div > div.v-card-text > div.v-table.v-table--has-top.v-table--has-bottom.v-theme--PurpleTheme.v-table--density-default.v-data-table > div.v-table__wrapper > table > tbody > tr:nth-child(3) > td:nth-child(1) > label > i').click()
     })
     it("delete company",()=>{
-        cy.get('#v-menu-17 > div > div > div').click()
-        cy.get('body > div.swal2-container.swal2-center.swal2-backdrop-show > div')
-        cy.get('body > div.swal2-container.swal2-center.swal2-backdrop-show > div > div.swal2-actions > button.swal2-confirm.swal2-styled').click()
+        cy.get('div[class="v-list-item v-list-item--link v-theme--PurpleTheme v-list-item--density-compact v-list-item--one-line rounded-0 v-list-item--variant-text cursor-pointer"]').wait(2000).click()
+        cy.get('button[class="swal2-confirm swal2-styled"]').click()
     })
 })
 
