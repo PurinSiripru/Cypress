5beb170376a4c169442b3f4b212a6c0f5b3a8320
PurinSiripru <122721492+PurinSiripru@users.noreply.github.com>
2024-12-12 09:17:39 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,8 @@
-const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://vending-main-client-dev.rvvcs.com/auth/login',
-    setupNodeEvents(on, config) {
-      on('file:preprocessor' ,cucumber())
-    },
     specPattern: "cypress/e2e/*.spec.js",
     defaultCommandTimeout: 20000,
     testIsolation: false,
